f0b4871a83674add625244b0a24a0393f5811082
Arwa-Abboud <arwaynad@gmail.com>
2024-12-30 22:42:27 +0000

    this is the first model trial

--- train_model.py
+++ train_model.py
@@ -55,41 +55,42 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 eng_padded = pad_sequences(eng_tokenized, maxlen=max_eng, padding='post')
 fr_padded = pad_sequences(fr_tokenized, maxlen=max_fr, padding='post')
 
-#build the model 2
+#build the model 1
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, RepeatVector, TimeDistributed, Bidirectional, Input
+from tensorflow.keras.layers import Embedding, LSTM, Dense, RepeatVector, TimeDistributed
 max_seq_len_fr = max(fr_tokenized_lengths)
 max_seq_len_eng = max(eng_tokenized_lengths)
-model2 = Sequential()
-model2.add(Embedding(eng_vocab, 128, mask_zero=True, input_shape=(max_seq_len_eng,)))
-model2.add(Bidirectional(LSTM(256, return_sequences=False), merge_mode='concat'))
-model2.add(RepeatVector(max_seq_len_fr))
-model2.add(Bidirectional(LSTM(256, return_sequences=True), merge_mode='concat'))
-model2.add(TimeDistributed(Dense(fr_vocab, activation='softmax')))
+model = Sequential()
+#encoder
+model.add(Embedding(input_dim=eng_vocab, output_dim=128, input_shape=(max_seq_len_eng,)))
+model.add(LSTM(256,  return_sequences=False)) #I added return_sequences=False in the encoder LSTM (meaning the model will output only the last hidden state)
+# RepeatVector to replicate the encoder output for each time step of the decoder
+model.add(RepeatVector(max_seq_len_fr))
+# Decoder
+model.add(LSTM(256,  return_sequences=True)) # true which allows it to generate sequences
+model.add(TimeDistributed(Dense(fr_vocab, activation='softmax')))
 
 
-model2.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
-model2.summary()
+model.summary()
 
-
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-import matplotlib.pyplot as plt
+from keras.callbacks import  EarlyStopping
 
 early_stop = EarlyStopping(monitor = 'val_accuracy', patience = 10)
-final_model2 = model2.fit(eng_padded, fr_padded, epochs=50, batch_size=512, validation_split = 0.3, callbacks=[early_stop])
+final_model = model.fit(eng_padded, fr_padded, epochs=20, batch_size=512, validation_split = 0.2, callbacks=[early_stop])
 
-
-def translate2(input, tokenizer):
+def translate(input, tokenizer):
     input = [input]
     test_tokenized = eng_token.texts_to_sequences(input)
     test_padded = pad_sequences(test_tokenized, maxlen=max_seq_len_eng, padding='post')
 
-    predictions = model2.predict(test_padded)[0]
+    predictions = model.predict(test_padded)[0]
 
     index_to_words = {id: word for word, id in tokenizer.word_index.items()}
     index_to_words[0] = ''
     return ' '.join([index_to_words[prediction] for prediction in np.argmax(predictions, 1)])
+
 
 input = "she is driving the truck"
 
@@ -98,4 +99,5 @@ input = "she is driving the truck"
 
 print('English: '+input)
 print('French: elle conduit le camion')
-print('Predicted: '+translate2(input, fr_token))
+print('Predicted: '+translate(input,  fr_token))
+model.save('model1_translator.h5')
